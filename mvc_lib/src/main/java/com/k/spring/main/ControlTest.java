a2511c9b4eba8935d4a640e759b640037dbca8ce
k <k@qq.com>
2019-09-09 11:13:04 +0800

    1

---
+++ mvc_lib/src/main/java/com/k/spring/main/ControlTest.java
@@ -0,0 +1,11 @@
+package com.k.spring.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlTest {
+    public static void main(final String[] args) {
+        SpringApplication.run(ControlTest.class, args);
+    }
+}
