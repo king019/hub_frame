b712b9696aee56a8be731d5c8a6836cc19fd9c7e
k <k@qq.com>
2019-12-04 01:56:27 +0800

    1

--- gc_cms/src/main/java/com/k/spring/boot/control/BootControl.java
+++ gc_cms/src/main/java/com/k/spring/boot/control/BootControl.java
@@ -2,15 +2,13 @@ package com.k.spring.boot.control;
 
 import com.k.spring.common.GCUtil;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/")
 public class BootControl {
-    @PostMapping("/")
-    @ResponseBody
+    @GetMapping("/")
     public String home() {
         GCUtil.xm();
         return "";
