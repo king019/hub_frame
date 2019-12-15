4bcd841094a999f1a9e8e2d56d33ce8bc9577e7d
k <k@qq.com>
2019-12-15 14:43:05 +0800

    1

--- gc_common/src/main/java/com/k/spring/boot/control/BootControl.java
+++ gc_common/src/main/java/com/k/spring/boot/control/BootControl.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/")
 public class BootControl {
-    private String ret = "ok";
+    private Object ret =new Object();
 
     @GetMapping("/")
     public Object home() {
